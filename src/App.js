2293f1e6209c8df3cf79e2afc435d3d9d762e7fa
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-03-11 09:36:57 +0530

    created the restraunts page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import LandingScreen from './screens/LandingScreen.js'
 import QuoteScreen from './screens/QuoteScreen.js';
+import RestrauntsScreen from './screens/RestrauntsScreen.js';
 
 function App() {
   return (
     <>
       {/* <LandingScreen/> */}
-      <QuoteScreen/>
+      {/* <QuoteScreen/> */}
+      <RestrauntsScreen/>
     </>
   );
 }
